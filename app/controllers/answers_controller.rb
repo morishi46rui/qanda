1e681c2dd5f642e1776f524b9444bdf1f99451ab
宮森瑠偉 <miyamorirui@miyamoriruinoMacBook-Air.local>
2023-02-23 17:01:17 +0900

    回答機能の追加

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,18 @@
 class AnswersController < ApplicationController
   def create
+    @question = Question.find(params[:question_id])
+    @question.answers.create(answer_params)
+    redirect_to question_path(@question)
   end
 
   def destroy
   end
 
   def edit
+  end
+
+  private
+  def answer_params
+    params.require(:answer).permit(:name, :content)
   end
 end

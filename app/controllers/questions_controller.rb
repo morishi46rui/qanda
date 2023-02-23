f3033f4e8d4c110abc5c8ad04cb4e12dd004d44a
宮森瑠偉 <miyamorirui@miyamoriruinoMacBook-Air.local>
2023-02-23 16:01:18 +0900

    投稿削除機能を実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -34,6 +34,9 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
   end
 
   private
